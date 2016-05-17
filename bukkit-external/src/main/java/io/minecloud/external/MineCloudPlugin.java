c29961e340719ba714aa78a1aa366b9a5e6d7de7
BlueFusion12 <bluefusion95@gmail.com>
2016-05-17 18:44:58 -0400

    Fix external plugin package name

--- bukkit-external/src/main/java/io/minecloud/external/MineCloudPlugin.java
+++ bukkit-external/src/main/java/io/minecloud/external/MineCloudPlugin.java
@@ -13,7 +13,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-package io.minecloud.bukkit;
+package io.minecloud.external;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
